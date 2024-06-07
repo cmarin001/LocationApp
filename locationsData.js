61e1604b992a820d6ce685e7a20655fdde83610a
cmarin001 <cmarin6051@gmail.com>
2024-06-07 18:11:02 -0500

    feat: update format for images

--- locationsData.js
+++ locationsData.js
@@ -3,216 +3,216 @@ const locationsData = {
     {
       place_id: "ny1",
       display_name: "Central Park, New York",
-      image_url: "/images/ny1.jpg"
+      image_url: "/images/ny1.png"
     },
     {
       place_id: "ny2",
       display_name: "Times Square, New York",
-      image_url: "/images/ny2.jpg"
+      image_url: "/images/ny2.png"
     },
     {
       place_id: "ny3",
       display_name: "Statue of Liberty, New York",
-      image_url: "/images/ny3.jpg"
+      image_url: "/images/ny3.png"
     },
     {
       place_id: "ny4",
       display_name: "Empire State Building, New York",
-      image_url: "/images/ny4.jpg"
+      image_url: "/images/ny4.png"
     },
     {
       place_id: "ny5",
       display_name: "Brooklyn Bridge, New York",
-      image_url: "/images/ny5.jpg"
+      image_url: "/images/ny5.png"
     }
   ],
   "Germany": [
     {
       place_id: "de1",
       display_name: "Brandenburg Gate, Berlin",
-      image_url: "/images/de1.jpg"
+      image_url: "/images/de1.png"
     },
     {
       place_id: "de2",
       display_name: "Neuschwanstein Castle, Bavaria",
-      image_url: "/images/de2.jpg"
+      image_url: "/images/de2.png"
     },
     {
       place_id: "de3",
       display_name: "Cologne Cathedral, Cologne",
-      image_url: "/images/de3.jpg"
+      image_url: "/images/de3.png"
     },
     {
       place_id: "de4",
       display_name: "Berlin TV Tower, Berlin",
-      image_url: "/images/de4.jpg"
+      image_url: "/images/de4.png"
     },
     {
       place_id: "de5",
       display_name: "Heidelberg Castle, Heidelberg",
-      image_url: "/images/de5.jpg"
+      image_url: "/images/de5.png"
     }
   ],
   "Paris": [
     {
       place_id: "pa1",
       display_name: "Eiffel Tower, Paris",
-      image_url: "/images/pa1.jpg"
+      image_url: "/images/pa1.png"
     },
     {
       place_id: "pa2",
       display_name: "Louvre Museum, Paris",
-      image_url: "/images/pa2.jpg"
+      image_url: "/images/pa2.png"
     },
     {
       place_id: "pa3",
       display_name: "Notre-Dame Cathedral, Paris",
-      image_url: "/images/pa3.jpg"
+      image_url: "/images/pa3.png"
     },
     {
       place_id: "pa4",
       display_name: "Arc de Triomphe, Paris",
-      image_url: "/images/pa4.jpg"
+      image_url: "/images/pa4.png"
     },
     {
       place_id: "pa5",
       display_name: "Sacré-Cœur Basilica, Paris",
-      image_url: "/images/pa5.jpg"
+      image_url: "/images/pa5.png"
     }
   ],
   "Tokyo": [
     {
       place_id: "to1",
       display_name: "Shibuya Crossing, Tokyo",
-      image_url: "/images/to1.jpg"
+      image_url: "/images/to1.png"
     },
     {
       place_id: "to2",
       display_name: "Tokyo Tower, Tokyo",
-      image_url: "/images/to2.jpg"
+      image_url: "/images/to2.png"
     },
     {
       place_id: "to3",
       display_name: "Sensō-ji Temple, Tokyo",
-      image_url: "/images/to3.jpg"
+      image_url: "/images/to3.png"
     },
     {
       place_id: "to4",
       display_name: "Tokyo Skytree, Tokyo",
-      image_url: "/images/to4.jpg"
+      image_url: "/images/to4.png"
     },
     {
       place_id: "to5",
       display_name: "Meiji Shrine, Tokyo",
-      image_url: "/images/to5.jpg"
+      image_url: "/images/to5.png"
     }
   ],
   "Sydney": [
     {
       place_id: "sy1",
       display_name: "Sydney Opera House, Sydney",
-      image_url: "/images/sy1.jpg"
+      image_url: "/images/sy1.png"
     },
     {
       place_id: "sy2",
       display_name: "Sydney Harbour Bridge, Sydney",
-      image_url: "/images/sy2.jpg"
+      image_url: "/images/sy2.png"
     },
     {
       place_id: "sy3",
       display_name: "Bondi Beach, Sydney",
-      image_url: "/images/sy3.jpg"
+      image_url: "/images/sy3.png"
     },
     {
       place_id: "sy4",
       display_name: "Taronga Zoo, Sydney",
-      image_url: "/images/sy4.jpg"
+      image_url: "/images/sy4.png"
     },
     {
       place_id: "sy5",
       display_name: "Darling Harbour, Sydney",
-      image_url: "/images/sy5.jpg"
+      image_url: "/images/sy5.png"
     }
   ],
   "London": [
     {
       place_id: "lo1",
       display_name: "Big Ben, London",
-      image_url: "/images/lo1.jpg"
+      image_url: "/images/lo1.png"
     },
     {
       place_id: "lo2",
       display_name: "Tower Bridge, London",
-      image_url: "/images/lo2.jpg"
+      image_url: "/images/lo2.png"
     },
     {
       place_id: "lo3",
       display_name: "Buckingham Palace, London",
-      image_url: "/images/lo3.jpg"
+      image_url: "/images/lo3.png"
     },
     {
       place_id: "lo4",
       display_name: "London Eye, London",
-      image_url: "/images/lo4.jpg"
+      image_url: "/images/lo4.png"
     },
     {
       place_id: "lo5",
       display_name: "British Museum, London",
-      image_url: "/images/lo5.jpg"
+      image_url: "/images/lo5.png"
     }
   ],
   "Rome": [
     {
       place_id: "ro1",
       display_name: "Colosseum, Rome",
-      image_url: "/images/ro1.jpg"
+      image_url: "/images/ro1.png"
     },
     {
       place_id: "ro2",
       display_name: "Trevi Fountain, Rome",
-      image_url: "/images/ro2.jpg"
+      image_url: "/images/ro2.png"
     },
     {
       place_id: "ro3",
       display_name: "St. Peter's Basilica, Vatican City",
-      image_url: "/images/ro3.jpg"
+      image_url: "/images/ro3.png"
     },
     {
       place_id: "ro4",
       display_name: "Pantheon, Rome",
-      image_url: "/images/ro4.jpg"
+      image_url: "/images/ro4.png"
     },
     {
       place_id: "ro5",
       display_name: "Roman Forum, Rome",
-      image_url: "/images/ro5.jpg"
+      image_url: "/images/ro5.png"
     }
   ],
   "San Francisco": [
     {
       place_id: "sf1",
       display_name: "Golden Gate Bridge, San Francisco",
-      image_url: "/images/sf1.jpg"
+      image_url: "/images/sf1.png"
     },
     {
       place_id: "sf2",
       display_name: "Alcatraz Island, San Francisco",
-      image_url: "/images/sf2.jpg"
+      image_url: "/images/sf2.png"
     },
     {
       place_id: "sf3",
       display_name: "Lombard Street, San Francisco",
-      image_url: "/images/sf3.jpg"
+      image_url: "/images/sf3.png"
     },
     {
       place_id: "sf4",
       display_name: "Fisherman's Wharf, San Francisco",
-      image_url: "/images/sf4.jpg"
+      image_url: "/images/sf4.png"
     },
     {
       place_id: "sf5",
       display_name: "Golden Gate Park, San Francisco",
-      image_url: "/images/sf5.jpg"
+      image_url: "/images/sf5.png"
     }
   ]
 };
