852cddd53e36a2479b2007d9d05f7aedd2870fe1
cmarin001 <cmarin6051@gmail.com>
2024-06-07 18:16:06 -0500

    fix: update express configuration

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const locationsData = require('./locationsData');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/locations', (req, res) => {
   const { query } = req.query;
