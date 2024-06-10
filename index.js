f23f51acb58208cd8476b2e1b54cfe6aea72c684
cmarin001 <cmarin6051@gmail.com>
2024-06-10 17:14:51 -0500

    fix: update index headers configuration

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+  res.setHeader('Cross-Origin-Opener-Policy', 'same-origin-allow-popups');
   res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
   next();
 });
